ea1011ee355d270bcce9e137db9b85523157b77b
Ketlina Puci <ketlina.puci@evolutivo.io>
2024-07-20 19:16:04 +0200

    Delete products feature

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -77,6 +77,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return redirect()->route('products.index');
     }
 }
